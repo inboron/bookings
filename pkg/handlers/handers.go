2bb93cd000fe0c45635193ea8bf55bec794fe82f
inboron590@gmail.com <syukatu97019a@gmail.com>
2024-03-27 16:53:31 +0000

    Initial Commit

---
+++ pkg/handlers/handers.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"chanel/pkg/config"
+	"chanel/pkg/models"
+	"chanel/pkg/render"
+	"net/http"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// New Handlers sets the repository for the handers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+
+}
+
+// About is the about page handler
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+
+	// perform some logic
+	stringMap := make(map[string]string)
+
+
+	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+
+}
